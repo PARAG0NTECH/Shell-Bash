16f8c2ef17f526c98c7899cc643d0863f49328cc
samuel lucena de abreu <samuellucenadeabreu@hotmail.com>
2023-11-25 10:11:32 -0300

    Novo script

--- script.sh
+++ script.sh
@@ -1,72 +1,83 @@
 #!/bin/bash
 
-# Verificar se o Java está instalado
-java -version
-
+sudo apt update
 if [ $? -eq 0 ]; then
-    echo "Java instalado"
-else
-    echo "Java não instalado"
-    echo "Gostaria de instalar o Java? [s/n]"
-    read get
-
-    if [ "$get" == "s" ]; then
-        # Instalar o Java 11
-        sudo apt-get update
-        sudo apt-get install openjdk-11-jdk -y
-    else
-        echo "O Java não foi instalado."
-        exit 1  # Saia do script com código de erro
-    fi
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/checker-qual-3.12.0.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/error_prone_annotations-2.11.0.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/failureaccess-1.0.1.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/guava-31.1-jre.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/j2objc-annotations-1.3.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/jna-5.10.0.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/jna-platform-5.10.0.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/jsr305-3.0.2.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/listenablefuture-9999.0-empty-to-avoid-conflict-with-guava.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/looca-api-2.2.0.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/mssql-jdbc-12.4.2.jre11.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/mysql-connector-j-8.1.0.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/oshi-core-6.1.5.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/protobuf-java-3.21.9.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/slf4j-api-1.7.30.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/slf4j-simple-1.7.30.jar
+    curl -O -L https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/teste_cinecine_jar/teste-cinecine.jar
 fi
-
-Docker --version
-
+sleep 5
 if [ $? -eq 0 ]; then
-    echo "Docker instalado"
-else
-    echo "Docker não instalado"
-    echo "Gostaria de instalar o Docker? [s/n]"
-    read get
 
-    if [ "$get" == "s" ]; then
-        # Instalar o Docker 
-        
-        sudo apt install Docker.io -y
-        sudo systemctl start docker
-        sudo systemctl enable docker
-    else
-        echo "O Docker não foi instalado."
-        exit 1  # Saia do script com código de erro
-    fi
-fi
+    docker --version
 
-    #criando um container Docker
-
-
-    sudo docker pull mysql:5.7
-    sudo docker images
-
-    # criando o container de MySQL
-    sudo docker run -d -p 3306:3306 --name ContainerBD -e "MYSQL_DATABASE=banco1" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
-
-    # Confirmando se foir criado
-    sudo docker ps -a
-
-     #Monitorando o consumo de recurso
-     sudo docker stats ContainerBD
-
-     #Para terminar o monitoramento, pressione “ctrl + c”
-
-     
-
-
-    # Baixar o arquivo cineproject.jar usando curl
-    curl -o cineproject.jar -LJO https://github.com/PARAG0NTECH/JAR-PROJETO/raw/main/cineproject.jar
-
-    # Verificar o status de saída do curl
     if [ $? -eq 0 ]; then
-    java -jar cineproject.jar
+        echo "Docker instalado"
     else
-    echo "O curl encontrou um erro."
+        echo "Docker não instalado"
+        echo "Gostaria de instalar o Docker? [s/n]"
+        read get
+
+        if [ "$get" == "s" ]; then
+            # Instalar o Docker
+            sudo apt install docker.io -y
+            sudo systemctl start docker
+            sudo systemctl enable docker
+        else
+            echo "O Docker não foi instalado."
+            exit 1  # Saia do script com código de erro
+        fi
     fi
+
+    # Verificar se o Java está instalado
+    java -version
+
+    if [ $? -eq 0 ]; then
+        echo "Java instalado"
+    else
+        echo "Java não instalado"
+        echo "Gostaria de instalar o Java? [s/n]"
+        read get
+
+        if [ "$get" == "s" ]; then
+            # Instalar o Java 11
+            sudo apt-get update
+            sudo apt-get install openjdk-17-jdk -y
+        else
+            echo "O Java não foi instalado."
+            exit 1  # Saia do script com código de erro
+        fi
+    fi
+    
+    cd ../..
+    cd var/run
+    
+    sudo mkdir mysqld 
+    sudo touch mysqld.sock
+
+    if [ $? -eq 0 ]; then
+        sudo docker run -d --name mysql-container -e MYSQL_ROOT_PASSWORD=my-secret -e MYSQL_DATABASE=cineguardian -p 3306:3306 mysql:8
+        sleep 10
+        sudo docker exec -i mysql-container mysql -uroot -pmy-secret cineguardian < setup-script.sql
+    fi
+    if [ $? -eq 0 ]; then
+        sleep 10   
+        java -jar teste-cinecine.jar SETUP
+    fi
+else
+    echo "Erro nos curls"
+fi
